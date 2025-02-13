9bd927edb987d4d8f389291fdc02aca08634ea95
Joao Pedro Pelet <joaopelet@unipam.edu.br>
2025-02-13 12:18:07 -0300

    feat(setup): Setting up development environment

---
+++ app/(auth)/welcome/styles.ts
@@ -0,0 +1,9 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export const StyledSafeAreaView = styled(SafeAreaView)`
+    display: flex;
+    height: 100%;
+    align-items: center;
+    justify-content: space-between;
+    background: #fff;
+`;
---
+++ app/(auth)/welcome/welcome.tsx
@@ -0,0 +1,14 @@
+import { Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Onboarding = () => {
+    return (
+        <SafeAreaView>
+            <TouchableOpacity>
+                <Text>Skip</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    );
+};
+
+export default Onboarding;
